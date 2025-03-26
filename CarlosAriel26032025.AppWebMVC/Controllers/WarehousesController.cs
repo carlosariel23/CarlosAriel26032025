87f8b27a622b975799298bc5feea63723e2fb8f2
Ariel <Ariel@DESKTOP-JK4J511>
2025-03-26 10:55:51 -0600

    se agregearon diseño ala visastas y se agrego la panginacion y busqueda

---
+++ CarlosAriel26032025.AppWebMVC/Controllers/WarehousesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarlosAriel26032025.AppWebMVC.Models;
+
+namespace CarlosAriel26032025.AppWebMVC.Controllers
+{
+    public class WarehousesController : Controller
+    {
+        private readonly Test20250326DbContext _context;
+
+        public WarehousesController(Test20250326DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(Warehouse bodega, string? warehouseName, int topRegistro = 10, int page = 1)
+        {
+            var query = _context.Warehouses.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(bodega?.WarehouseName))
+            {
+                query = query.Where(s => s.WarehouseName.Contains(bodega.WarehouseName));
+            }
+            if (!string.IsNullOrWhiteSpace(warehouseName)) 
+            {
+                query = query.Where(w => w.WarehouseName.Contains(warehouseName));
+            }
+            var totalCount = await query.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalCount / (double)topRegistro); 
+            query = query.Skip((page - 1) * topRegistro).Take(topRegistro);
+            var warehouses = await query.ToListAsync();
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
+            return View(warehouses);
+        }
+
+
+
+
+        // GET: Warehouses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Warehouses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,WarehouseName")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warehouse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,WarehouseName")] Warehouse warehouse)
+        {
+            if (id != warehouse.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warehouse);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarehouseExists(warehouse.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse != null)
+            {
+                _context.Warehouses.Remove(warehouse);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WarehouseExists(int id)
+        {
+            return _context.Warehouses.Any(e => e.Id == id);
+        }
+    }
+}
--- CarlosAriel26032025.AppWebMVC/Models/Warehouse.cs
+++ CarlosAriel26032025.AppWebMVC/Models/Warehouse.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarlosAriel26032025.AppWebMVC.Models;
 
 public partial class Warehouse
 {
     public int Id { get; set; }
-
+    [Display(Name = "Nombre del Almacén")]
+    [Required(ErrorMessage = "El nombre del almacén es obligatorio.")]
     public string WarehouseName { get; set; } = null!;
-
+    [Display(Name = "Productos")]
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
