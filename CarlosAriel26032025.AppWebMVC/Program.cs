b640a90400229da6405887371fe885e1eb0893ae
Ariel <Ariel@DESKTOP-JK4J511>
2025-03-26 08:00:08 -0600

    se modifico el archivo program y appsethings

--- CarlosAriel26032025.AppWebMVC/Program.cs
+++ CarlosAriel26032025.AppWebMVC/Program.cs
@@ -1,7 +1,14 @@
+using CarlosAriel26032025.AppWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Test20250326DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
