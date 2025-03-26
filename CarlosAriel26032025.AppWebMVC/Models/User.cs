2099ab727ab1b5e2d2a62c358945abb511c4454b
Ariel <Ariel@DESKTOP-JK4J511>
2025-03-26 07:52:55 -0600

    se hizo la nexcion ala base de datos a git hub y se instalaron los paquetes nugets

---
+++ CarlosAriel26032025.AppWebMVC/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarlosAriel26032025.AppWebMVC.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
